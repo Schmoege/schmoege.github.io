ab82d042da950f9ba0b079ff1c4ddf1dba452633
Schmoege <christoffer.isacsson@gmail.com>
2017-11-15 19:12:20 +0100

    Init

---
+++ script.js
@@ -0,0 +1,100 @@
+var api = "bb05488111608f09bfd5f0c89099b773";
+var levels = "";
+var adress = "https://www.wanikani.com/api/user/"+api+"/kanji/"+levels;
+var chosenKanji = []
+var tilesToEndGame;
+var amountCorrectTiles = 0;
+var lastPressedTile;
+
+var app = angular.module('kanjiApp', []);
+app.controller('kanjiCtrl', function($scope, $http, $timeout) {
+    $http.get(adress)
+    .then(function (response) {
+        var shuffledResponse = shuffle(response.data.requested_information);
+        var slicedResponse = shuffledResponse.slice(0,8);
+
+        var kanjiArray = [];
+        var meaningArray = [];
+        
+        for(var i = 0; i<slicedResponse.length; i++) {
+            var kanjiObject = {character: slicedResponse[i].character, clicked: false, correct: false, tileId : i, fail: false};
+            var meaningObject = {character: slicedResponse[i].meaning, clicked: false, correct: false, tileId : i, fail: false};
+            kanjiArray.push(kanjiObject);
+            meaningArray.push(meaningObject);
+        }
+
+        var scopeArray = shuffle(kanjiArray.concat(meaningArray));
+        
+        tilesToEndGame = scopeArray.length;
+        $scope.kanji = scopeArray});
+
+        $scope.toggleActive = function(chosenTile){
+            if(chosenTile != lastPressedTile) {
+                chosenKanji.push(chosenTile);
+                chosenTile.clicked = true;
+                lastPressedTile = chosenTile;
+                compareKanjiInArray($timeout);
+            }
+        }
+});
+
+function compareKanjiInArray($timeout){
+    
+    if (chosenKanji.length === 2) {
+        var tilesMatch = chosenKanji[0].tileId === chosenKanji[1].tileId
+        if(tilesMatch) {
+            chosenKanji.forEach(function(tile) {
+                tile.correct=true;
+                amountCorrectTiles++;
+                chosenKanji = [];
+            }, this);
+            if(amountCorrectTiles === tilesToEndGame){
+                window.alert("You are winner!");
+                $scope.gameOver=true;
+            }
+        }
+        else{
+            chosenKanji[0].fail = true;
+            chosenKanji[1].fail = true;
+            $timeout(function(){
+                resetFailedTiles();
+                lastPressedTile = undefined;
+                chosenKanji[0].fail = false;
+                chosenKanji[1].fail = false;
+                chosenKanji = [];
+            },1000);
+        }
+          //Clears array
+    } 
+}
+
+function toggleFailColour(){
+
+}
+
+function resetFailedTiles() {
+    chosenKanji.forEach(function(tile) {
+        tile.clicked=false;
+    }, this);
+}
+
+
+//Fisher-Yates shuffle
+function shuffle(array) {
+    let counter = array.length;
+    //let = scope är i functionen
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+    
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+    return array;
+}
